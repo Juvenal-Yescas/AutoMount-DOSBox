d17ea80c37c2570943e8e015b062aa7d717eda03
Juve-yescas <juvenal@mail.com>
2017-03-13 11:39:17 -0600

    Add .bat to .exe - Readme and fix erros cr

--- cr.bat
+++ cr.bat
@@ -4,8 +4,8 @@ echo %arg1%.exe >> files.txt
 del c:\Masm\%arg1%.exe
 cd Masm
 ml c:/projects/%arg1%.asm
-del c:\Masm\%arg1%.LNK
-del c:\Masm\%arg1%.OBJ
+if exist c:\Masm\%arg1%.LNK del c:\Masm\%arg1%.LNK
+if exist c:\Masm\%arg1%.OBJ del c:\Masm\%arg1%.OBJ
 if exist c:\Masm\%arg1%.exe cls
 c:\Masm\%arg1%.exe 
 cd..
