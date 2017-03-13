3edffec91788a0d55289ae687aa8da5828e39c97
Juve-yescas <juvenal@mail.com>
2017-03-13 09:28:22 -0600

    Add files

---
+++ cr.bat
@@ -0,0 +1,12 @@
+@ECHO OFF
+set arg1=%1
+echo %arg1%.exe >> files.txt
+del c:\Masm\%arg1%.exe
+cd Masm
+ml c:/projects/%arg1%.asm
+del c:\Masm\%arg1%.LNK
+del c:\Masm\%arg1%.OBJ
+cls
+c:\Masm\%arg1%.exe 
+cd..
+echo.
