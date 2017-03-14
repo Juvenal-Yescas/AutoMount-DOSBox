aff50865fd9d3777b8a92309ee991adf7ae880c3
Juve-yescas <juvenal@mail.com>
2017-03-14 09:37:31 -0600

    add if exist .exe to run

--- cr.bat
+++ cr.bat
@@ -7,6 +7,6 @@ ml c:/projects/%arg1%.asm
 if exist c:\Masm\%arg1%.LNK del c:\Masm\%arg1%.LNK
 if exist c:\Masm\%arg1%.OBJ del c:\Masm\%arg1%.OBJ
 if exist c:\Masm\%arg1%.exe cls
-c:\Masm\%arg1%.exe 
+if exist c:\Masm\%arg1%.exe %arg1%.exe
 cd..
 echo.
